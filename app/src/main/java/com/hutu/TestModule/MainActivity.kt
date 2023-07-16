4637a6ef4841f16451cd86dc20d60e9337a2cced
flyflowsmallblank <2669655226@qq.com>
2023-07-16 13:25:35 +0800

    :sparkles: 多模块测试

---
+++ app/src/main/java/com/hutu/TestModule/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hutu.TestModule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
